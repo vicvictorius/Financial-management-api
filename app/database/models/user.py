2bfa2c8a41906294e564dc6050112023fda43caa
vicvictorius <victoreduardop387@gmail.com>
2026-09-12 22:46:54 -0300

    fix: corrige autenticação do postgres no docker e cria tabela users

---
+++ app/database/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(150),
+        unique=True,
+        nullable=False,
+        index=True
+    )
