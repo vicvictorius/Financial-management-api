7bde81358c7047313fbc4e8a499b79ad2468ac5d
vicvictorius <victoreduardop387@gmail.com>
2026-09-12 23:15:50 -0300

    adding user tables

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,17 +1,36 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Interger, String, DataTime
+from sqlaclchemy.sql import func
 
 from app.database.database import Base
-
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    email: Mapped[str] = mapped_column(
-        String(150),
+    id = Column(
+        Interger,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
+
+    email = Column(
+        String(255),
         unique=True,
         nullable=False,
         index=True
+    )
+
+    password_hash = Column(
+        String(255),
+        nullable=False
+    )
+
+    created_at = Column(
+        DataTime(timezone=True),
+        server_default=func.now()
     )
